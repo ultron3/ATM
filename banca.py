ca90e20dbb479b74c52f28f0d24e59c5c1807750
alex gnavi <alex.gnavi@gmail.com>
2023-02-28 14:20:44 +0100

    aggiunto commento

--- banca.py
+++ banca.py
@@ -1,5 +1,7 @@
 #il codice lo preso da python.hub
 #io ho aggiunto il phone recharge e il deposit
+#nella riga 19 verrà richiesto il pin, il pin è (1234)
+
 while True:
     balance=10000
     print(" ATM  Unicredit Bank")
