4acc3560a925025dff8bae919c5fc27323321e74
alex gnavi <alex.gnavi@gmail.com>
2023-03-01 14:31:33 +0100

    setting json

--- login_banca/index.js
+++ login_banca/index.js
@@ -14,7 +14,7 @@ console.log("script ok");
 
 
 function generateOTP(){
-    var digits = '0123456789';
+    var digits = '724942';   // '0123456789'
     let OTP='';
     for (let i =0; i<6; i++){
         OTP +=digits[Math.floor(Math.random()*10)]
