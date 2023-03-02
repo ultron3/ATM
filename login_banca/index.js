8ba607eb13ef36f408c8f4648c045f0039b54d4a
alex gnavi <alex.gnavi@gmail.com>
2023-03-02 13:28:31 +0100

    Funzione login

--- login_banca/index.js
+++ login_banca/index.js
@@ -24,3 +24,34 @@ function generateOTP(){
 
 generateOTP() // '724942'
 generateOTP()// '761294'
+
+
+function login() {
+    // Ottenere i valori inseriti dall'utente
+    const username = document.getElementById("Username").value;
+    const password = document.getElementById("Password").value;
+    
+    // Verificare se i valori inseriti sono validi
+    if (username === "" || password === "") {
+      alert("Inserisci un username e una password validi");
+      return false;
+    }
+    
+    // Effettuare la chiamata AJAX per verificare le credenziali dell'utente
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "/login");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        const response = JSON.parse(xhr.responseText);
+        if (response.success) {
+          // L'utente è autenticato, effettua il redirect alla homepage
+          window.location.href = "/homepage";
+        } else {
+          alert("Credenziali errate");
+        }
+      }
+    };
+    xhr.send(JSON.stringify({username: username, password: password}));
+  }
+  
