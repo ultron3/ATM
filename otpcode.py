7da9369f99ef74e8897f5170604d0b04077e1bef
alex gnavi <alex.gnavi@gmail.com>
2023-03-02 10:49:35 +0100

    codice per genare otp può servire per qualcosa in futuro

---
+++ otpcode.py
@@ -0,0 +1,9 @@
+import random
+
+def genotp(digit):
+    otp=random.randrange(1,10)
+    for otp in range(digit-1):
+        otp=(otp+10)+random.randrange(0,10)
+        return otp
+otp1=genotp(6)
+print(otp1)
