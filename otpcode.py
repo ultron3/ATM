a08c10e18c0578a55a28ea4f0f176ac6b7d21f0c
alex gnavi <alex.gnavi@gmail.com>
2023-03-02 10:56:47 +0100

    funzione più chiara

--- otpcode.py
+++ otpcode.py
@@ -1,11 +1,11 @@
 import random
 
-def genotp(digit):
+def gen_otp(digit):
     otp=random.randrange(1,10)
     for otp in range(digit-1):
         otp=(otp+10)+random.randrange(0,10)
         return otp
-otp1=genotp(6)
+otp1=gen_otp(6)
 print(otp1)
 
 
