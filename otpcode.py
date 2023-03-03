a0df85f296106550018be912095b203f8efe93d4
alex gnavi <alex.gnavi@gmail.com>
2023-03-03 11:56:54 +0100

    modifica commenti

--- otpcode.py
+++ otpcode.py
@@ -1,6 +1,6 @@
 import random
 
-def gen_otp(digit):
+def gen_otp(digit):     #generazione otp
     otp=random.randrange(1,10)
     for otp in range(digit-1):
         otp=(otp+10)+random.randrange(0,10)
@@ -9,4 +9,4 @@ otp1=gen_otp(6)
 print(otp1)
 
 
-#una funzione esiste anche in index.js
+#questa funzione è scritta anche in javascript in index.js
